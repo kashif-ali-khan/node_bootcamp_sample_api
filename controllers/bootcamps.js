d6ece43f254df21c5cc22a2d3fbc97979178f963
kashif khan <kashif.khan@tothenew.com>
2023-07-19 15:06:53 +0530

    Paging: Adding pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,21 +1,95 @@
 const Bootcamp = require("./../models/Bootcamp");
+const asyncHandler = require("./../middleware/async");
 
 // @desc     Get All Bootcamps
 // @route    /api/v1/bootcamps
 // @access   public
 
-exports.getAllBootcamps = async (req, res, next) => {
-  try {
-    const bootcamps = await Bootcamp.find();
-    res.status(200).json({
-      success: true,
-      count: bootcamps.length,
-      data: bootcamps,
-    });
-  } catch (error) {
-    next(error);
+exports.getAllBootcamps = asyncHandler(async (req, res, next) => {
+  //const query = req.query;
+
+  const reqQuery = { ...req.query };
+
+  const removeField = ["select", "sort", "limit", "page"];
+
+  removeField.forEach((field) => delete reqQuery[field]);
+
+  let queryString = JSON.stringify(reqQuery);
+  queryString = queryString.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  );
+  console.log(queryString);
+
+  let query = Bootcamp.find(JSON.parse(queryString));
+
+  // Select fields
+  if (req.query.select) {
+    const selectFields = req.query.select
+      .split(",")
+      .map((name) => name.trim())
+      .join(" ");
+    query = query.select(selectFields);
   }
-};
+
+  // Sort By
+  if (req.query.sort) {
+    const sortField = req.query.sort.split(",").join(" ");
+    query = query.sort(sortField);
+  }
+
+  // Pagination
+
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit,10) || 1;
+
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  query = query.skip(startIndex).limit(limit);
+
+  // Pagination Data
+  const total = await Bootcamp.countDocuments();
+
+  // Executing query
+  const bootcamps = await query;
+
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page-1,
+      limit,
+    };
+  }
+
+  res.status(200).json({
+    success: true,
+    count: total,
+    pagination,
+
+    data: bootcamps,
+  });
+  // exports.getBootcamps = asyncHandler(async (req, res, next) => {
+  //     res.status(200).json(res.advancedResults);
+  //   });
+  //   try {
+  //     const bootcamps = await Bootcamp.find();
+  //     res.status(200).json({
+  //       success: true,
+  //       count: bootcamps.length,
+  //       data: bootcamps,
+  //     });
+  //   } catch (error) {
+  //     next(error);
+  //   }
+});
 
 // @desc     Get  Bootcamps By Id
 // @route    /api/v1/bootcamps/:id
@@ -39,7 +113,6 @@ exports.getBootcampsById = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
- 
 };
 
 // @desc     Create  Bootcamps
