0fd176528ac810c261cf25f4bb5eb1be3ed276c9
kashif khan <kashif.khan@tothenew.com>
2023-07-19 15:50:16 +0530

    Course: Created Course Schema

--- seeder.js
+++ seeder.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const mongoose = require("mongoose");
 const Bootcamp = require("./models/Bootcamp");
+const Course = require("./models/Course");
 const dotenv = require("dotenv");
 const colors = require("colors");
 
@@ -12,9 +13,14 @@ const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
 );
 
+const courses = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
+  );
+
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log("Imported".green.inverse);
     process.exit(1);
   } catch (error) {
@@ -25,6 +31,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log("Deleted".red.inverse);
     process.exit(1);
   } catch (error) {
